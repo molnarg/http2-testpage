627d9ef6ac66594e5829ddd4e808b81b74b9f856
Gábor Molnár <gabor@molnar.es>
2013-09-10 15:51:31 +0200

    framing-bad-ping: implementing basic testcase

--- test/framing-bad-ping/index.js
+++ test/framing-bad-ping/index.js
@@ -1,8 +1,19 @@
 var http2 = require('http2');
 
 module.exports = function(options) {
-  var server = http2.createServer(options, function(req, res) {
+  var server = http2.createServer(options);
+
+  server.on('request', function(req, res) {
     res.end('Hello World!\n');
+  });
+
+  server.on('connection', function(socket, endpoint) {
+    endpoint._serializer.write({
+      type: 'PING',
+      flags: {},
+      stream: 0,
+      data: new Buffer(10)
+    });
   });
 
   return server;
