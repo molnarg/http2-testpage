a0694ab1731a3eb71d629ea98ab29e55f5618a55
Gábor Molnár <gabor@molnar.es>
2013-09-15 17:17:07 +0200

    framing-bad-ping: referencing the spec

--- test/framing-bad-ping/index.js
+++ test/framing-bad-ping/index.js
@@ -1,3 +1,15 @@
+// [From the spec](http://tools.ietf.org/html/draft-ietf-httpbis-http2-06#section-6.7):
+//
+//     In addition to the frame header, PING frames MUST contain 8 octets of
+//     data in the payload. A sender can include any value it chooses and
+//     use those bytes in any fashion.
+//
+//     ...
+//
+//     Receipt of a PING frame with a length field value other than 8 MUST
+//     be treated as a connection error (Section 5.4.1) of type
+//     PROTOCOL_ERROR.
+
 var http2 = require('http2');
 
 module.exports = function(socket, log, callback) {
